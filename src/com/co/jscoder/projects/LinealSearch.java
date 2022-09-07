6dfcf9a3cd2cf1d7516d0d96aaa2002b004c6f11
jscoder-pc <javier.ant.sanchezc@gmail.com>
2022-09-07 12:16:29 -0500

    :angry: esto es un cambio de pruebas

--- src/com/co/jscoder/projects/LinealSearch.java
+++ src/com/co/jscoder/projects/LinealSearch.java
@@ -1,14 +1,26 @@
 package com.co.jscoder.projects;
 
-import java.util.Arrays;
+
 import java.util.logging.Logger;
 
 public class LinealSearch {
-    static final Logger LOG = Logger.getLogger("paquete.NombreClase");
+    static Logger logger = Logger.getLogger("MyLog");
     public static void main(String[] args) {
         int[] array = { 10, 15, 20, 40, 50, 100, 120, 200, 400, 500, 600, 800, 2222 };
-        int result = Arrays.binarySearch(array, 400);
-       LOG.info("result = " + result + " bytes  " + result + " bytes " + result + " bytes " + result + " bytes");
+
+        for (int S :
+                array) {
+            logger.fine("Searching : {}"+S);
+
+        }
+
+
+
+
+
+
+
+
 
     }
 }
